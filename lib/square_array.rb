291aa65a36fbd4a4f45d9df0f1d3bd562a5125a2
Sophia Lyu <shunshunlyu@protonmail.com>
2020-01-30 00:28:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while array.length < counter do
-    array.push(array[counter]**array[counter])
+    array[counter] = array[counter]**array[counter]
     counter += 1
   end
   array
